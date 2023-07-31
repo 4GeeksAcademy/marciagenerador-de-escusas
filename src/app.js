87cccc6c98fb6bcf9f5c0642820c6bbdc3ac5807
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-07-31 15:20:01 +0000

    generador de escusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoRondon = who[Math.floor(Math.random() * who.length)];
+  let actionRandon = action[Math.floor(Math.random() * action.length)];
+  let whatRandon = what[Math.floor(Math.random() * what.length)];
+  let whenRandon = when[Math.floor(Math.random() * when.length)];
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let esxcusa =
+    whoRondon + " " + actionRandon + " " + whatRandon + " " + whenRandon;
+  console.log(esxcusa);
+  // function esxcusas() {
+  document.getElementById("excuse").innerHTML = esxcusa;
+  // }
 };
